ee3acd9f66b012f4add2619e6be563cc41de4add
kokaevao <alexander.kokaev@gmail.com>
2025-02-13 17:54:59 +0300

    update element tests added test: test_web_table_search_person test_web_table_add_person

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,8 @@ from selenium import webdriver
 @pytest.fixture(scope='function')
 def driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--incognito")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--allow-insecure-localhost")
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
